7c6c2a10e5f676e2e35e0aaa3883fa57fe16d29d
Mehdi Asadli <asadlimehdi25@gmail.com>
2026-02-10 09:47:35 +0400

    chore: update tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-	entry: ["src/index.ts"],
-	format: ["esm", "cjs"],
-	dts: true,
-	clean: true,
-	target: "esnext",
-});
+export default defineConfig([
+	{
+		entry: ["src/index.ts"],
+		format: ["esm", "cjs"],
+		dts: true,
+		clean: true,
+		target: "esnext",
+	},
+	{
+		entry: ["src/cli.ts"],
+		format: ["esm", "cjs"],
+		clean: false,
+		target: "esnext",
+		banner: {
+			js: "#!/usr/bin/env node",
+		},
+	},
+]);
